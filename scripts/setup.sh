6ffa3d079a416e4738ee938a6d2ac6b41cf47652
Shinichi Tomita <shinichi.tomita@gmail.com>
2023-07-10 17:14:23 +0900

    add setup shell script to shortcut initial setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-sfdx force:package:install --package 04t0K000001E4iS
-sfdx force:user:permset:assing --permsetname msmxSheet__MashmatrixSheetUser
-sfdx force:user:permset:assign --permsetname msmxSheet__MashmatrixSheetAdministrator
+sfdx force:org:create -f config/project-scratch-def.json -a msmx-sheet-integration -w 10
 
-sfdx force:source:push
-sfdx force:user:permset:assign --permsetname Mashmatrix_Sheet_Integration_Examples
+sfdx force:package:install --package 04t0K000001E4iS -u msmx-sheet-integration
+sfdx force:user:permset:assign --permsetname msmxSheet__MashmatrixSheetUser -u msmx-sheet-integration
+sfdx force:user:permset:assign --permsetname msmxSheet__MashmatrixSheetAdministrator -u msmx-sheet-integration
 
-sfdx force:org:open
+sfdx force:source:push -u msmx-sheet-integration
+sfdx force:user:permset:assign --permsetname Mashmatrix_Sheet_Integration_Examples -u msmx-sheet-integration
+
+sfdx plugins:install sfdx-migration-automatic
+
+sfdx automig:load --inputdir data/records -u msmx-sheet-integration
+sfdx automig:load --inputdir data/books -u msmx-sheet-integration
+
+sfdx force:org:open -u msmx-sheet-integration -p /lightning/n/msmxSheet__MashmatrixSheet
