18a72da04416577ffc0705652550b073c3b9ea65
Shinichi Tomita <shinichi.tomita@gmail.com>
2023-08-22 17:26:28 +0900

    show platform event message palyoads in debug messages

--- force-app/main/default/lwc/messageDebug/messageDebug.js
+++ force-app/main/default/lwc/messageDebug/messageDebug.js
@@ -3,9 +3,15 @@ import {
   APPLICATION_SCOPE,
   createMessageContext,
   releaseMessageContext,
-  subscribe,
-  unsubscribe
+  subscribe as subscribeMessageChannel,
+  unsubscribe as unsubscribeMessageChannel
 } from "lightning/messageService";
+import {
+  subscribe as subscribePlatformEvent,
+  unsubscribe as unsubscribePlatformEvent,
+  onError as onPlatformEventError,
+  isEmpEnabled,
+} from "lightning/empApi";
 import MC_SELECT_RECORDS from "@salesforce/messageChannel/msmxSheet__selectRecords__c";
 import MC_FOCUS_CELL from "@salesforce/messageChannel/msmxSheet__focusCell__c";
 import MC_LOAD_COMPLETE from "@salesforce/messageChannel/msmxSheet__loadComplete__c";
@@ -23,6 +29,12 @@ const MESSAGE_CHANNELS = {
   executeCommand: MC_EXECUTE_COMMAND
 };
 
+const PLATFORM_EVENTS = {
+  ExecuteQuery__e: "/event/msmxSheet__ExecuteQuery__e",
+  SaveRecords__e: "/event/msmxSheet__SaveRecords__e",
+  Download__e: "/event/msmxSheet__Download__e"
+};
+
 /**
  *
  */
@@ -33,35 +45,74 @@ export default class MessageDebug extends LightningElement {
 
   messages = [];
 
-  subscriptions = null;
+  subscriptions = [];
 
   get messageLogsEmpty() {
     return this.messages.length === 0;
   }
 
   connectedCallback() {
+    this._initMessageChannelSubscriptions();
+    this._initPlatformEventSubscriptions();
+  }
+
+  _initMessageChannelSubscriptions() {
     this.messageContext = createMessageContext();
-    this.subscriptions = Object.keys(MESSAGE_CHANNELS).map((channelName) => {
-      const subscr = subscribe(
-        this.messageContext,
-        MESSAGE_CHANNELS[channelName],
-        (message) => {
-          this.handleIncomingMessage("message-channel", channelName, message);
-        },
-        { scope: APPLICATION_SCOPE }
-      );
-      return {
-        unsubscribe: () => unsubscribe(subscr)
-      };
+    this.subscriptions = [
+      ...this.subscriptions,
+      ...Object.keys(MESSAGE_CHANNELS).map((channelName) => {
+        const subscr = subscribeMessageChannel(
+          this.messageContext,
+          MESSAGE_CHANNELS[channelName],
+          (message) => {
+            this.handleIncomingMessage("message-channel", channelName, message);
+          },
+          { scope: APPLICATION_SCOPE }
+        );
+        return {
+          unsubscribe: () => unsubscribeMessageChannel(subscr)
+        };
+      })
+    ];
+  }
+
+  async _initPlatformEventSubscriptions() {
+    const enabled = await isEmpEnabled();
+    if (!enabled) {
+      console.log('platform event listening is not enabled');
+      return;
+    }
+    this.subscriptions = [
+      ...this.subscriptions,
+      ...(await Promise.all(
+        Object.entries(PLATFORM_EVENTS).map(async ([channelName, channelPath]) => {
+          const subscr = await subscribePlatformEvent(
+            channelPath,
+            -1,
+            (message) => {
+              this.handleIncomingMessage(
+                "platform-event",
+                channelName,
+                message.data.payload
+              );
+            }
+          );
+          return {
+            unsubscribe: () => subscr && unsubscribePlatformEvent(subscr)
+          };
+        })
+      ))
+    ];
+    onPlatformEventError((error) => {
+      console.error('Received error from server: ', JSON.stringify(error));
     });
   }
 
   disconnectedCallback() {
-    if (this.subscriptions) {
-      for (const subscr of this.subscriptions) {
-        subscr.unsubscribe();
-      }
+    for (const subscr of this.subscriptions) {
+      subscr.unsubscribe();
     }
+    this.subscriptions = [];
     if (this.messageContext) {
       releaseMessageContext(this.messageContext);
       this.messageContext = null;
@@ -74,13 +125,14 @@ export default class MessageDebug extends LightningElement {
     this.messages = [
       {
         id: messageId,
+        messageType,
         messageClass: `message ${messageType}`,
-        channelName: channelName,
+        channelName,
         raw: message,
         text: JSON.stringify(message, null, 2),
-        componentId: message.componentId,
-        bookId: message.bookId,
-        sheetId: message.sheetId,
+        componentId: message.componentId ?? message.msmxSheet__ComponentId__c,
+        bookId: message.bookId ?? message.msmxSheet__BookId__c,
+        sheetId: message.sheetId ?? message.msmxSheet__SheetId__c,
         timestamp: new Date().toLocaleTimeString(),
         opened: false
       },
@@ -90,7 +142,6 @@ export default class MessageDebug extends LightningElement {
 
   handleMessageToggle(event) {
     const messageId = event.currentTarget.dataset.messageId;
-    console.log(event.currentTarget.dataset, event.detail);
     const message = this.messages.find((m) => m.id === messageId);
     if (message) {
       message.opened = !message.opened;
